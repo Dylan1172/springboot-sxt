f15a74596b91a69d26b152bc14ccd592a7580e15
Dylan1172 <1172474142>
2019-07-11 10:18:20 +0800

    转发到/error进行自适应响应效果处理
    Signed-off-by: Dylan1172 <1172474142>

--- spring-boot-04-web/src/main/java/com/dylan/handler/GlobalExceptionHandler.java
+++ spring-boot-04-web/src/main/java/com/dylan/handler/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import com.dylan.exception.MyException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -13,25 +12,34 @@ import java.util.Map;
 /**
  * 全局捕获异常
  *
- * @author: Dylan.W
- * @date: 2019年4月4日 下午11:08:20
+ * @Author Dylan.W
+ * @Date 2019/7/11 10:15
  */
 @Slf4j
 @ControllerAdvice(basePackages = "com.dylan.controller") // 作为全局异常处理的切面类，可以设置包的范围
-@ResponseBody
 public class GlobalExceptionHandler {
 
-
+    /**
+     * 处理自定义异常
+     * 视图跳转，不需要@ResponseBody
+     *
+     * @param e       自定义异常
+     * @param request 请求
+     * @return 视图跳转
+     */
     @ExceptionHandler(MyException.class)
-    public Map<String, Object> handleException(Exception e, HttpServletRequest request) {
+    public String handleException(Exception e, HttpServletRequest request) {
         System.out.println("*****自定义异常捕捉****");
+        e.printStackTrace();
         Map<String, Object> map = new HashMap<>();
         //传入我们自己的错误状态码  4xx 5xx，否则就不会进入定制错误页面的解析流程
         request.setAttribute("javax.servlet.error.status_code", 401);
         map.put("code", 400);
         map.put("message", e.getMessage());
-        return map;
+        // 重定向
+//        return "redirect:/error";
+        //转发到/error
+        return "forward:/error";
     }
-
 
 }
